70e17795670be6ce8b06fb8b3d112f8da1ef6b9f
leonardo <leonardoalvriv@gmail.com>
2022-02-14 19:36:30 -0600

    Primer commit de katas - actulizacion kata 10

--- Módulo 10/kata 10-Manejo Errores.py
+++ Módulo 10/kata 10-Manejo Errores.py
@@ -1,15 +1,13 @@
-
 def main():
     try:
-        imagen = open("config.txt")
-    except FileNotFoundError:
-        print("No se puedo encontrar el archivo")
+        imagen = open("/Users/leonardoalvarez/Documents/python/curso microsoft/Módulo 10/config.txt")
+        print("se encontro")
+    except FileNotFoundError as errFile:
+        print("No se puedo encontrar el archivo", errFile)
     except IsADirectoryError:
         print("archivo encontrado pero es un directorio, no se puede leer")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
 
 if __name__ == '__main__':
     main()
-
-
-
-
